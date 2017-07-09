e074ed276d83fcffd1e8db136f18266bb74b0711
Hung Do <hdhungdo@gmail.com>
2017-07-09 02:27:22 -0400

    'assas'

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -17,7 +17,7 @@ export class MembersComponent implements OnInit {
   name: any;
   state: string = '';
 
-  constructor(public af: AngularFireAuth,private router: Router) {
+  constructor(public af: AngularFireAuth, private router: Router) {
 
     this.af.authState.subscribe((auth) => {
       if(auth) {
